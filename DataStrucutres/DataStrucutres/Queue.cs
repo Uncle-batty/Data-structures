fe7380d5353e1e38e0166493a7fbc17b75179801
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2024-04-20 00:18:15 +0200

    Added queue
    added a array based queue

---
+++ DataStrucutres/DataStrucutres/Queue.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucutres
+{
+    internal class Queue<T>
+    {
+        List<T> list;
+        private int head;
+        private int tail;
+        public Queue()
+        {
+            list = new List<T>();
+            head = 0;
+            tail = 0;
+        }
+
+        public void enqueue(T val)
+        {
+            tail= tail + 1;
+            list.Add(val);
+        }
+
+        public T dequeue() 
+        {
+            if(isEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+            T DEITEM = list[head];
+            list.RemoveAt(head);
+            tail--;
+           
+            return DEITEM;
+        }
+
+        public bool isEmpty()
+        {
+            return list.Count == 0;
+        }
+
+        public T getHead() {
+            if (isEmpty())
+            {
+                throw new InvalidOperationException("Queue is empty.");
+            }
+
+            return list[head]; 
+        
+        }
+    }
+}
