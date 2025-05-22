2b30911cfd2d5f6e969762d7b5ad39246e9fea45
Louis batty <93161778+Uncle-batty@users.noreply.github.com>
2025-05-22 11:26:58 +0200

    USeQueues

--- DataStrucutres/DataStrucutres/Program.cs
+++ DataStrucutres/DataStrucutres/Program.cs
@@ -14,11 +14,18 @@ namespace DataStrucutres
             //Palindrome palindrome = new Palindrome("SSuss");
             //Console.WriteLine(palindrome.isplai);
 
-            Console.WriteLine(TwoSum([1,2,3,4,5,6,7], 8))
-            Console.ReadKey();
+            //Console.WriteLine(TwoSum([1, 2, 3, 4, 5, 6, 7], 8));
+            //Console.ReadKey();
+
+            var Queue = new UseQueues();
+
+            Queue.AddSong(0);
+            Queue.AddSong(1);
+
+            Queue.Playsongs();
         }
 
-        public int[] TwoSum(int[] nums, int target)
+        public static int[] TwoSum(int[] nums, int target)
         {
             for (int i = 0; i < nums.Length; i++)
             {
@@ -32,6 +39,23 @@ namespace DataStrucutres
             }
             return new int[] { -1, -1 };
         }
+
+        public int[] PlusOne(int[] digits)
+        {
+            for (int i = digits.Length - 1; i >= 0; i--) { 
+                if (digits[i] < 9) { digits[i] += 1; return digits; }
+                else { digits[i] = 0; }
+            }
+            int[] result = new int[digits.Length + 1];
+            result[0] = 1;  
+            return result;
+        }
+
+        //public LinkedListNode<int> DeleteLinkedListNodes(LinkedListNode<int> head)
+        //{
+        //    Dictionary<LinkedListNode<int>, int> values = new Dictionary<LinkedListNode<int>, int>();
+
+        //}
 
     }
 }
---
+++ DataStrucutres/DataStrucutres/UseQueues.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStrucutres
+{
+    public class UseQueues
+    {
+        List<Song> allSongs = new List<Song>
+{
+    new Song { Id = 0, Title = "Blinding Lights", Artist = "The Weeknd", Length = "3:20", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b"},
+    new Song { Id = 1, Title = "Shape of You", Artist = "Ed Sheeran", Length = "3:53", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 2, Title = "Bad Guy", Artist = "Billie Eilish", Length = "3:14" , Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b"},
+    new Song { Id = 3, Title = "Levitating", Artist = "Dua Lipa", Length = "3:23", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 4, Title = "Bohemian Rhapsody", Artist = "Queen", Length = "5:55", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 5, Title = "Uptown Funk", Artist = "Mark Ronson ft. Bruno Mars", Length = "4:30", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 6, Title = "Smells Like Teen Spirit", Artist = "Nirvana", Length = "5:01", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 7, Title = "Rolling in the Deep", Artist = "Adele", Length = "3:48", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 8, Title = "Take On Me", Artist = "a-ha", Length = "3:46", Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b" },
+    new Song { Id = 9, Title = "Billie Jean", Artist = "Michael Jackson", Length = "4:54" , Url = "https://open.spotify.com/track/0VjIjW4GlUZAMYd2vXMi3b"}
+};
+
+        Queue<Song> Songs;
+
+        public UseQueues()
+        {
+            Songs = new Queue<Song>();
+        }
+
+        public void AddSong(int id)
+        {
+            if (id >= 0 && id < allSongs.Count)
+                Songs.enqueue(allSongs[id]);
+            else
+                Console.WriteLine("Invalid song ID.");
+        }
+
+        public Song Playsongs()
+        {
+
+            var currentSong = Songs.getHead();
+
+            Console.WriteLine(currentSong.Title);
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = currentSong.Url,
+                UseShellExecute = true
+            });
+
+            Songs.dequeue();
+            return currentSong;
+
+        }
+    }
+
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Length { get; set; }
+        public string Url { get; set; }
+    }
+
+}
